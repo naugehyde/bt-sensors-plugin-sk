7f0e4f1d338500b382ed08e8bbcc315f619d02c8
naugehyde <oh.that.andy@gmail.com>
2024-09-25 12:23:52 -0700

    XIaomi decrypt

--- sensor_classes/Xiaomi.js
+++ sensor_classes/Xiaomi.js
@@ -1,5 +1,7 @@
 const BTSensor = require("../BTSensor");
 
+const crypto = require('crypto');
+
 class Xiaomi extends BTSensor{
     constructor(device,params){
         super(device,params)
@@ -11,7 +13,7 @@ class Xiaomi extends BTSensor{
         try{
             const sd = await device.getProp('ServiceData')
             if (sd == null || sd.length==0) return null
-            const keys = Object.keys(sd) 
+            const keys = Object.keys(sd)
             if (parseInt(keys[0].substring(0,8),16)==0xFE95) return this
         } catch (e){
             console.log(e)
@@ -35,7 +37,7 @@ class Xiaomi extends BTSensor{
             (buff,offset)=>{return ((buff.readUInt8(offset))/100)})
         this.addMetadatum('voltage', 'V',  'sensor battery voltage',
             (buff,offset)=>{return ((buff.readUInt16LE(offset))/1000)})
-        
+       
     }
 
     #emitValues(buffer){
@@ -49,11 +51,45 @@ class Xiaomi extends BTSensor{
         const gattService = await gattServer.getPrimaryService("ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6")
         this.gattCharacteristic = await gattService.getCharacteristic("ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6")
     }
+
+    decrypt(data){
+        const encryptedPayload = data.subarray(11,-7);
+        const xiaomi_mac = data.subarray(5,11)
+        const nonce = Buffer.concat([xiaomi_mac, data.subarray(2, 5), data.subarray(-7,-4)]);
+        const cipher = crypto.createDecipheriv('aes-128-ccm', Buffer.from(this.bindKey,"hex"), nonce, { authTagLength: 4});
+        cipher.setAAD(Buffer.from('11', 'hex'), { plaintextLength: encryptedPayload.length });
+        cipher.setAuthTag(data.subarray(-4))    
+        return cipher.update(encryptedPayload)
+    }
+
+    async #connectEncrypted(){
+        this.cb=async()=>{
+            const sd = await this.device.getProp("ServiceData")
+            const dec = this.decrypt(sd["0000fe95-0000-1000-8000-00805f9b34fb"].value)
+            switch(dec[0]){
+            case 0x04:    
+                console.log("temp")
+                this.emit("temp",(dec.readInt16LE(3)/10)+273.15)  
+                break        
+            case 0x06:
+                console.log("humidity")
+                this.emit("humidity",(dec.readInt16LE(3)/10))          
+                break
+            default:
+                console.log("wait wha?"+ dec[0])
+            }
+        }
+        await this.cb()
+        this.device.helper.on("PropertiesChanged",async (props)=>{
+            await this.cb()
+        })
+    }
    
     #setupEmission(){
+   
         if (this.pollFreq){
             this.device.disconnect().then(()=>{
-                this.intervalID = setInterval( async () => {             
+                this.intervalID = setInterval( async () => {            
                     try{
                         await this.#connect()
                         const buffer = await this.gattCharacteristic.readValue()
@@ -70,7 +106,7 @@ class Xiaomi extends BTSensor{
                 })
             }
         else {
-           this.gattCharacteristic.startNotifications().then(()=>{     
+           this.gattCharacteristic.startNotifications().then(()=>{    
                 this.gattCharacteristic.on('valuechanged', buffer => {
                     this.#emitValues(buffer)
                 })
@@ -78,12 +114,15 @@ class Xiaomi extends BTSensor{
         }
     }
     async connect() {
-        
-        await this.#connect()
-        this.gattCharacteristic.readValue().then((buffer)=>{
-            this.#emitValues(buffer)
-            this.#setupEmission()
-        })
+        if (this.bindKey)
+            this.#connectEncrypted()
+        else{
+            await this.#connect()
+            this.gattCharacteristic.readValue().then((buffer)=>{
+                this.#emitValues(buffer)
+                this.#setupEmission()
+            })
+        }
         return this
     }
     async #disconnectGattCharacteristic(){
@@ -95,7 +134,9 @@ class Xiaomi extends BTSensor{
     async disconnect(){
         super.disconnect()
         await this.#disconnectGattCharacteristic()
-        
+        if (this.cb){
+            this.device.helper.removeListener("PropertiesChanged",this.cb)
+        }
         if (this.intervalID){
             clearInterval(this.intervalID)
         }

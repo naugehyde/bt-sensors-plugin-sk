3dc7cd8ddaef4022d8c18d07a098eb95eca7680b
ArjenR <arjenrr@gmail.com>
2025-02-27 12:02:00 +0100

    Initial support for BleTrackers.

--- sensor_classes/BleTracker.js
+++ sensor_classes/BleTracker.js
@@ -1,22 +1,21 @@
 const BTSensor = require("../BTSensor");
-class BleTracker extends BTSensor{
-    static isSystem = true
+class BleTracker extends BTSensor {
+    static isSystem = true;
 
-    static async identify(device){
-        const name = await this.getDeviceProp(device,"Name")
-        const uuids = await this.getDeviceProp(device,'UUIDs')
-        return this
+    static async identify(device) {
+        const appearance = await this.getDeviceProp(device, "Appearance");
+        if (appearance == 0x0240) {
+            return this;
+        }
+        return null;
     }
-    async init(){
-        await super.init()
-        this.currentProperties.Name= `Tracking: ${this.getManufacturer()}`
+    async init() {
+        await super.init();
+        this.currentProperties.Name = `Tracking: ${this.getManufacturer()}, ${this.getName()}, ${this.getDisplayName()}`;
         // var md =this.addMetadatum("bleTracker",'db',"BLE Tracker")
         // md.isParam=true
         // md.enum=Array.from(this.constructor.classMap.keys())
         // md.enumNames=this.constructor.classMap
-
     }
-
-
 }
-module.exports=BleTracker
+module.exports = BleTracker;

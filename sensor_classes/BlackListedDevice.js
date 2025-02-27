604b496132aeb4a9c5b308809ab8d1213e3c45ea
ArjenR <arjenrr@gmail.com>
2025-02-27 11:59:52 +0100

    work around for WGX_iBeacon which makers have seem to have partly copied Apple iPhone manufacturer information

--- sensor_classes/BlackListedDevice.js
+++ sensor_classes/BlackListedDevice.js
@@ -1,31 +1,34 @@
 const BTSensor = require("../BTSensor");
-class BLACKLISTED extends BTSensor{
-    static isSystem = true
-    static async identify(device){
-        const md = await this.getDeviceProp(device,'ManufacturerData')
-        if (md){
-            const keys = Object.keys(md)
-            if (keys.length==1 && keys[0]==0x004C)
-                return this
+class BLACKLISTED extends BTSensor {
+    static isSystem = true;
+    static async identify(device) {
+        const md = await this.getDeviceProp(device, "ManufacturerData");
+        const ap = await this.getDeviceProp(device, "Appearance"); // For keyfinder with appearance 0x0240
+        if (md) {
+            const keys = Object.keys(md);
+            if (keys.length == 1 && keys[0] == 0x004c && ap != 0x0240) {
+                return this;
+            }
         }
-        return null
+        return null;
     }
-    async init(){
-        await super.init()
-        this.currentProperties.Name= `Unsupported device from ${this.getManufacturer()}`
+    async init() {
+        await super.init();
+        this.currentProperties.Name = `Unsupported device from ${this.getManufacturer()}`;
     }
     reasonForBlacklisting() {
-        switch ( this.getManufacturerID()){
-            case (0x004C): return "Randomized MAC address (Apple)"
-            case (0x02e1): return "Device is using VE.Smart" //NOTE: Victron/VictronSensor class 
-                                                             //determines if a device is using VE.Smart
-                                                             //in identify(). If so, identify() returns
-                                                             //BlackListedDevice
-        
-            default: return ""
-            
-        }
+        switch (this.getManufacturerID()) {
+            case 0x004c:
+                return "Randomized MAC address (Apple)";
+            case 0x02e1:
+                return "Device is using VE.Smart"; //NOTE: Victron/VictronSensor class
+            //determines if a device is using VE.Smart
+            //in identify(). If so, identify() returns
+            //BlackListedDevice
 
+            default:
+                return "";
+        }
     }
 }
-module.exports=BLACKLISTED
+module.exports = BLACKLISTED;

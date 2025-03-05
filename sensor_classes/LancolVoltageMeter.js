7add7501bff63528d89a20db5cf1094e277b8c77
naugehyde <oh.that.andy@gmail.com>
2025-03-05 09:10:56 -0800

    Lancol fix redux

--- sensor_classes/LancolVoltageMeter.js
+++ sensor_classes/LancolVoltageMeter.js
@@ -27,7 +27,7 @@ class LancolVoltageMeter extends BTSensor{
         const regex = /^[0-9]{1,3}\.[0-9]{1,2}V/
         
         if (props.Name) {
-            the.emitData("voltage",parseFloat(props.Name.match(regex)))
+            this.emitData("voltage",parseFloat(props.Name.match(regex)))
         }
    }                         
 }

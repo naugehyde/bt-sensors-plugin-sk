3af34eaa2f126dafc86c90d7a38f74c9837e4cbd
naugehyde <oh.that.andy@gmail.com>
2025-07-27 22:38:18 -0700

    EctiveBMS

--- sensor_classes/EctiveBMS.js
+++ sensor_classes/EctiveBMS.js
@@ -1,20 +1,64 @@
 const BTSensor = require("../BTSensor");
 
-/*
-"5e353433373030303030303030303030303438453830313030313030303633303043303042303038303837463433413045303530453033304530463044303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303537440000"*/
+const testData=[
+  ["5e 45 39 33 36 30 30 30 30 31 44 46 34 46 46 46 46",
+  "34 30 30 44 30 33 30 30 30 46 30 30 36 33 30 30",
+  "43 38 30 42 30 30 38 30 30 37 42 34",
+  "42 46 30 44 41 43 30 44 42 42 30 44 43 33 30 44",
+  "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+  "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+  "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+  "30 41 39 42 00 00 00 00 00 00 00 00"],
+  ["5e 35 34 33 37 30 30 30 30 30 30 30 30 30 30 30 30",
+   "34 38 45 38 30 31 30 30 31 30 30 30 36 33 30 30",
+   "43 30 30 42 30 30 38 30 38 37 46 34",
+   "33 41 30 45 30 35 30 45 30 33 30 45 30 46 30 44",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 35 37 44 00 00 00 00 00 00"
+  ],
+  //bad data (!=0x21)
+  ["5e 35 34 33 37 30 30 30 30 30 30 30 30 30 30 30 30", 
+   "34 38 45 38 30 21 30 30 31 30 30 30 36 33 30 30", 
+   "43 30 30 42 30 30 38 30 38 37 46 34",
+   "33 41 30 45 30 35 30 45 30 33 30 45 30 46 30 44",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30",
+   "30 35 37 44 00 00 00 00 00 00"
+  ]
+]
+
 class EctiveBMS extends BTSensor {
   static Domain = BTSensor.SensorDomains.electrical
 
-  static RX_SERVICE = "0000ff00-0000-1000-8000-00805f9b34fb"  
-  static NOTIFY_CHAR_UUID = "0000ff04-0000-1000-8000-00805f9b34fb"
-  
+  static RX_SERVICE = "0000ffe0-0000-1000-8000-00805f9b34fb"  
+  static NOTIFY_CHAR_UUID = "0000ffe4-0000-1000-8000-00805f9b34fb"
+  static test(data, numCells=4){
+    const obj = new EctiveBMS()
+    obj.currentProperties={Name:"Topbrand BMS", Address:"<mac>"}
+    obj.numberOfCells=numCells
+    obj.initSchema()
+    obj.debug=(m)=>{console.log(m)}
+    for (const [tag,path] of Object.entries(obj._schema.properties.paths.properties)) {
+      obj.on(tag, val=>{console.log(`${tag} => ${val} `)})
+    }
+    data.forEach(d => {
+      const _d=d.replaceAll(" ","").toUpperCase()
+      const b=Buffer.from(_d,"hex")
+
+      obj.updateBuffer(b)
+    });
+  }
   static identify(device){
     return null
   }
 
   _dataBuffer=null
+  _dataError = false
 
-  async initSchema(){
+  initSchema(){
       super.initSchema()
       this.addDefaultParam("batteryID")
       this.addParameter(
@@ -29,42 +73,46 @@ class EctiveBMS extends BTSensor {
      
       this.addDefaultPath('voltage','electrical.batteries.voltage')
         .read=
-        (buffer)=>{return buffer.readUInt16LE(0) / 1000}
+        (buffer)=>{return buffer.readUInt32LE(0) / 1000}
 
       this.addDefaultPath('current','electrical.batteries.current')
         .read=
-        (buffer)=>{return buffer.readInt16LE(2) / 1000} 
+        (buffer)=>{return buffer.readInt32LE(4) / 1000} 
 
        this.addMetadatum(`cycleCharge`, 'C', `cycle charge`,
           (buffer)=>{
-            return 3600*(buffer.readUInt16LE(4)/1000)
+            return 3600*(buffer.readUInt32LE(8)/1000)
           })
-         .default=`electrical.batteries.{batteryID}.cycleCharge`
+         .default='electrical.batteries.{batteryID}.cycleCharge'
 
-      
-      this.addDefaultPath('remainingCapacity','electrical.batteries.capacity.remaining')
-        .read=(buffer)=>{return (buffer.readUInt16BE(8) / 100)*3600} 
-      
-      this.addDefaultPath('capacity','electrical.batteries.capacity.actual')
-        .read=(buffer)=>{return (buffer.readUInt16BE(10) / 100)*3600} 
-     
-      this.addDefaultPath('cycles','electrical.batteries.cycles' )
-        .read=(buffer)=>{return buffer.readUInt16BE(12)} 
+
+       this.addMetadatum('cycleCount', '', 'number of cycles',
+          (buffer)=>{
+            return buffer.readUInt16LE(12)
+          })
+         .default='electrical.batteries.{batteryID}.cycles'
 
       this.addDefaultPath('SOC','electrical.batteries.capacity.stateOfCharge')
-        .read=(buffer)=>{return buffer.readUInt8(15)/100} 
+        .read=(buffer)=>{return buffer.readUInt16LE(14)/100} 
       
-        this.addMetadatum(`temp`, 'K', `Temperature reading`,
+        this.addMetadatum('temp', 'K', 'Temperature reading',
           (buffer)=>{
-            return buffer.readUInt16BE(27+(i*2))/10
+            return buffer.readUInt16LE(16)/10
           })
-         .default=`electrical.batteries.{batteryID}.temperature`
+         .default='electrical.batteries.{batteryID}.temperature'
+
+
+        this.addMetadatum('flags', '', 'Problem flags',
+          (buffer)=>{
+            return buffer.readUInt16LE(18).toString('2').padStart(16,'0')
+          })
+         .default='electrical.batteries.{batteryID}.problemFlags'
       
       
       for (let i=0; i<this?.numberOfCells??4; i++){
-        this.addMetadatum(`cell${i}Voltage`, 'V', `Cell ${i+1} voltage`,
-          (buffer)=>{return buffer.readUInt16BE((4+(i*2)))/1000} )
-          .default=`electrical.batteries.{batteryID}.cell${i}.voltage`
+        this.addMetadatum(`cell${i+1}Voltage`, 'V', `Cell ${i+1} voltage`,
+          (buffer)=>{return buffer.readUInt16LE((22+(i*2)))/1000} )
+          .default=`electrical.batteries.{batteryID}.cell${i+1}.voltage`
       }
   }
 
@@ -72,16 +120,31 @@ class EctiveBMS extends BTSensor {
     return true
   }
 
+  verifyData(d1,d2){
+          if (d1.length!==d2.toString("utf8").length/2) {
+            this._dataError=true
+            this.debug(`Malformed packet received: ${d2.toString()}`)
+          }
+          return !this._dataError
+  }
+
   updateBuffer(data){
-    const _data = Buffer.from(data.toString("utf-8"),"hex")
-            if (_data[0]==0x5E){
-                this._dataBuffer=_data.subarray(1)
-            }
+
+            if (data[0]==0x5E){ //start of stream
+                this._dataBuffer = Buffer.from(data.subarray(1).toString("utf8"),"hex") 
+                this.verifyData(this._dataBuffer,data.subarray(1))
+            } 
             else {
-                this._dataBuffer=Buffer.concat([this._dataBuffer, _data])
-            }
-            if (_data.includes(0x00)){
-                this.emitValuesFrom(this._dataBuffer)
+              if (!this._dataError) {
+                const zeroIndex = data.indexOf(0x0)
+                const d = zeroIndex==-1?data:data.subarray(0,zeroIndex)
+                const _data = Buffer.from(d.toString("utf8"),"hex")
+                if (this.verifyData(_data,d)) {
+                  this._dataBuffer=Buffer.concat([this._dataBuffer, _data])
+                  if (zeroIndex>1) //end of stream
+                    this.emitValuesFrom(this._dataBuffer)
+                }
+              }
             }
     }
 

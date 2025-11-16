8199ff2c31ea2ae251e235589b6c8e0daa467f56
Patrick Guélat <pg@imp.ch>
2025-11-16 17:40:20 +0000

    correct acceleration (is reported in 2-complement m/s2 )

--- sensor_classes/MikrotikTag.js
+++ sensor_classes/MikrotikTag.js
@@ -44,9 +44,9 @@ class MikrotikTag extends BTSensor{
     // 08 1 UserData     (xx)  00 => not encrypted, 01 => encrypted
     // Secrect-Field is used for payload, multibyte values always in little endian
     // 09 2 Salt         (xxxx) used as checksum for payload, if the same in two message, data is unchanged
-    // 11 2 X-Accel      (xxxx) m/s2
-    // 13 2 Y-Accel      (xxxx) m/s2
-    // 15 2 Z-Accel      (xxxx) m/s2
+    // 11 2 X-Accel      (xxxx) m/s2 (*256)
+    // 13 2 Y-Accel      (xxxx) m/s2 (*256)
+    // 15 2 Z-Accel      (xxxx) m/s2 (*256)
     // 17 2 Temperature  (xxxx) C * 256
     // 19 4 Uptime       (xxxxxxxx) seconds
     // 23 1 Flags        (1:FLAG_REED_SWITCH,2:FLAG_ACCEL_TILT,3:FLAG_ACCEL_FREE_FALL,4:FLAG_IMPACT_X,5:FLAG_IMPACT_X,6:FLAG_IMPACT_Z,7:FLAG_ACCEL_DISABLED)
@@ -57,16 +57,16 @@ class MikrotikTag extends BTSensor{
             .read=(buffer)=>{ return parseFloat((273.15+buffer.readInt16LE(10)/256.0).toFixed(3))}
 
         this.addMetadatum("accX","m/s2","acceleration on X-axis", 
-            (buffer)=>{ return buffer.readInt16LE(4)}
+            (buffer)=>{ return parseFloat((buffer.readInt16LE(4)/256.0).toFixed(3)) }
         ).examples=["sensors.{macAndName}.accX"]
         
         this.addMetadatum("accY","m/s2","acceleration on Y-axis", 
-            (buffer)=>{ return buffer.readInt16LE(6)}
-        )        .examples=["sensors.{macAndName}.accY"]
+            (buffer)=>{ return parseFloat((buffer.readInt16LE(6)/256.0).toFixed(3)) }
+        ).examples=["sensors.{macAndName}.accY"]
         
         this.addMetadatum("accZ","m/s2","acceleration on Z-axis", 
-            (buffer)=>{ return buffer.readInt16LE(8)}
-        ) .examples=["sensors.{macAndName}.accZ"]
+            (buffer)=>{ return parseFloat((buffer.readInt16LE(8)/256.0).toFixed(3)) }
+        ).examples=["sensors.{macAndName}.accZ"]
 
         this.addDefaultPath("battery","sensors.batteryStrength")
             .read=(buffer)=>{ return parseFloat((buffer[17]/100.0).toFixed(3)) }
